122440d073c85dca4ee3c9c133c5dd5d9bd3fb9d
Vinh Le <letanvinh0210@gmail.com>
2021-04-07 00:26:53 -0700

    Update index.js

--- project1/html/scripts/index.js
+++ project1/html/scripts/index.js
@@ -1,132 +1,133 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDXwzhjBRafKkta_SDSsRZHw_7Bvhm28q0",
-    authDomain: "team-projec.firebaseapp.com",
-    projectId: "team-projec",
-    storageBucket: "team-projec.appspot.com",
-    messagingSenderId: "434710215462",
-    appId: "1:434710215462:web:0b65cc627d33b9d0531fe7",
-    measurementId: "G-W72V0CV5W9"
+  apiKey: "AIzaSyDXwzhjBRafKkta_SDSsRZHw_7Bvhm28q0",
+  authDomain: "team-projec.firebaseapp.com",
+  projectId: "team-projec",
+  storageBucket: "team-projec.appspot.com",
+  messagingSenderId: "434710215462",
+  appId: "1:434710215462:web:0b65cc627d33b9d0531fe7",
+  measurementId: "G-W72V0CV5W9"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-
-let file = {};
+//Getting the user profile picture's link-------------------------------
+// It's in sign-up.html
+var file = {};
 function chooseFile(e){
   file = e.target.files[0];
+  if(file == '{}'){
+    console.log('file is null');
+  }
 }
 
-// Check if user is currently logged in or not
-firebase.auth().onAuthStateChanged(function(user) {
+// write user data in realtime database--------------------------
+// it's in sign-up.html
+function writeUserData(userId, name, email, description) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    description: description
+  });
+}
+
+// show user information
+// for debug purpose
+function Authentication_checking(){
+  firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
-
       // Debug purpose-------------------------
-      // console.log(user);
-      // console.log(user.displayName);
-
-      // Upload image-------------------------------------------
-      var storage = firebase.storage();
-      var pathReference = storage.ref('users/' + user.uid + '/profile.jpg');
-      pathReference.getDownloadURL()
-        .then(function(url){
-          console.log(url);
-          // img used the id in sign-up.html, but we can change it later
-          var img = document.getElementById('profileImage');
-          img.setAttribute('src', url);
-        })
-        .catch(function(error){
-          console.log(error);
-        });    
-
-    } else {
+      console.log(user);
+      console.log('currently logged in as ' + user.email);
+    } 
+    else {
       // No user is signed in.
+      console.log('No user is currently logged in');
     }
   });
-
-//Loggin user ----------------------------------------------------------------
-function login(){
-    'use strict';
-    var userEmail = document.getElementById("inputEmail").value;
-    var userPassword = document.getElementById("inputPassword").value;
-
-    if(userEmail === "" || userPassword === "") {
-      var noInput = new Error('Please enter email and password');
-      window.alert(noInput);
-    }
-    else {
-      firebase.auth().signInWithEmailAndPassword(userEmail, userPassword)
-      .then(function(userCredential){
-          var user = userCredential.user;
-          // location.href redirect user to other pages
-          window.location.href = "result.html";
-          console.log(user);
-      })
-      .catch(function(error){
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          window.alert("Error: " + errorMessage);
-      });
-    }
-
-    event.preventDefault();
 }
 
-// Create an Account for user---------------------------------------------
-function createAccount(){
-    'use strict';
-    var name = document.getElementById("inputName").value;
-    var description = document.getElementById("inputDescription").value;
-    var email = document.getElementById("inputEmail").value;
-    var password = document.getElementById("inputPassword").value;
 
-    firebase.auth().createUserWithEmailAndPassword(email, password)
+// Login function------------------------------------------------
+// In login.html, it's linked with signed in button
+function login(){
+  'use strict';
+  var userEmail = document.getElementById("inputEmail").value;
+  var userPassword = document.getElementById("inputPassword").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPassword)
   .then(function(userCredential){
-    // Signed in 
-    var user = firebase.auth().currentUser;
-    user.updateProfile({
-      displayName: name
-    })
-    console.log(user.displayName);
-
-    //Store image-----------------------------------------------
-    firebase.storage().ref('users/' + user.uid + '/profile.jpg').put(file).then(function(){
-      console.log("sucessfully uploaded picture");
-
-    })
-    .catch(function(error){
-      window.alert("Error: " + error.message);
-    });
-    // -------------------------------------------------------------
-    
-    window.location.href = "result.html";
+      var user = userCredential.user;
+      // location.href redirect user to other pages
+      window.location.href = "result.html";
+      console.log(user);
   })
   .catch(function(error){
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    window.alert("Error: " + errorMessage);
-    // ..
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error: " + errorMessage);
   });
   event.preventDefault();
 }
 
-// We need Signout button for user to sign out
-function signOut(){
-    'use strict';
-    firebase.auth().signOut().then(() => {
-      window.alert('Signed Out');
-    }) .catch((error) => {
-      window.alert('Error logging out');
-    });
-    
+// This function is to check if user is currently logged in or not
+// I tried to link this function to our login button but i failed
+function check_user(){
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      window.location.href = 'result.html';
+    } else {
+      // No user is signed in.
+      window.location.href = 'login.html';
+    }
+  });
 }
 
-// function uploadfile(files){
-//   var storageRef = firebase.storage().ref();
-//   var proRef = storageRef.child('users/profile.jpg');
+// sign-up function-----------------------------------------
+// In sign-up.html, it's linked with create an Account button
+function createAccount(){
+  'use strict';
+  var name = document.getElementById("inputName").value;
+  var description = document.getElementById("inputDescription").value;
+  var email = document.getElementById("inputEmail").value;
+  var password = document.getElementById("inputPassword").value;
 
-//   var file = files.item(0);
-//   var task = proRef.put(file);
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+.then(function(userCredential){
+  // Signed in 
+  var user = userCredential.user;
 
-// }
+  // Store image-----------------------------------------------
+  firebase.storage().ref('users/' + user.uid + '/profile.jpg').put(file).then(function(){
+    console.log("sucessfully uploaded piture");
+  })
+  .catch(function(error){
+    window.alert("Error: " + error.message);
+  });
+  // -------------------------------------------------------------
+  
+  // store bio, it call writeUser data function above
+  writeUserData(user.uid, name, email, description);
+})
+.catch(function(error){
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  window.alert("Error: " + errorMessage);
+  // ..
+});
+event.preventDefault();
+}
+
+// signout function------------------------------------------
+// In our result.html, linked it with our
+function signOut(){
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    // Once the user sign out, we will redirect them back to main page, which is about.html
+    window.location.href = 'about.html';
+  }).catch(function(error){
+    // An error happened.
+  });
+}
